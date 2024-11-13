c163510b35979cff773fc910564be14ce7961dff
AdityaPray <adityaprayoga1216@gmail.com>
2024-11-13 19:13:08 +0700

    pembaruan

--- javascript.js
+++ javascript.js
@@ -49,3 +49,18 @@ $(document).ready(function () {
     const year = new Date().getFullYear();
     document.getElementById("footer").querySelector(".footer-bottom p").innerHTML = `&copy; ${year} Pariwisata Indonesia. All rights reserved.`;
 });
+//fungsi untuk menjalankan contact
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    //Simulasikan pengiriman formulir
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    
+    // Tampilkan pesan sukses
+    const formMessage = document.getElementById("formMessage");
+    formMessage.textContent = `Terima kasih, ${nama}! Kami telah menerima pesan Anda dan akan segera menghubungi Anda kembali di ${email}.`;
+    
+    document.getElementById("contactForm").reset();
+});
